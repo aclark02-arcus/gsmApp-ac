917ae9e89ba8b41b035cba9eabd43638f8beee35
Aaron Clark (Arcus) <aclark02@arcusbio.com>
2024-03-27 08:36:58 -0400

    add code used to download stuff, add manifest:

--- app.R
+++ app.R
@@ -1,7 +1,28 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
+
+# pkgs_req <- c('bslib', 'htmltools', 'shiny', 'cli', 'dplyr', 'DT', 'here', 'qs',
+#               'shinyjs', 'htmlwidgets', 'pkgload', 'purrr', 'rlang', 'stringr',
+#               'usethis', 'tidyr')
+# invisible(sapply(pkgs_req, function(x){
+#     if(length(find.package(x, quiet = TRUE)) == 0) {
+#         message(paste('Installing package:', x))
+#         install.packages(x)
+#     }}))
+#
+# ## Install and load gsm
+# devtools::install_github("Gilead-BioStats/gsm", ref = "main")
+#
+# ## Install and load sample raw data
+# devtools::install_github("Gilead-BioStats/clindata", ref = "main", force = TRUE)
+#
+# ## Install and load sample SDTM and AdAM data
+# install.packages('safetyData')
+
+
 pkgload::load_all(".")
 
 gsmApp::run_app()
 
+rsconnect::writeManifest()
